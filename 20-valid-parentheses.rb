9fdc7a43001bde8ddd6f408da0c7f380006f96ff
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-10-04 15:35:26 -0500

    added some changes to valid parentheses

--- 20-valid-parentheses.rb
+++ 20-valid-parentheses.rb
@@ -1,5 +1,5 @@
 
-
+require "debug"
 
 def valid_parentheses(string)
   stack = []
@@ -7,33 +7,34 @@ def valid_parentheses(string)
   start = Time.now
 
   string.each_char do |char|
+    # debugger
     if close_to_open.values.include?(char)
       stack.push(char)
     elsif close_to_open.keys.include?(char)
-      if stack.empty? || stack.pop != close_to_open[char]
+      if stack.empty? || stack[-1] != close_to_open[char]
         return false
       end
+      stack.pop
     end
   end
 
   finish = Time.now
   diff = finish - start
   pp format('Hash Method: %.6f seconds', diff)
-
   return stack.empty?
 end
 
 
 
+string = "(({}))"
 
 
 
 
 
-
-def is_valid_brute_force(s)
+def is_valid_gsub_method(s)
   # Initialize the length of the string
-  start = Time.now
+  # start = Time.now
   previous_length = 0
 
   # Loop until no more pairs can be removed
@@ -46,16 +47,18 @@ def is_valid_brute_force(s)
     s.gsub!("[]", "")
   end
 
-  finish = Time.now
-  diff = finish - start
-  pp format('Brute Force Method: %.6f seconds', diff)
-
+  # finish = Time.now
+  # diff = finish - start
+  # pp format('Gsub Method: %.6f seconds', diff)
 
   # If the string is empty, it's valid
   s.empty?
 end
 
+string = "(({}))"
 
-string = "({[({[({[(){}]})]})]})" * 10000000
 
-pp valid_parentheses(string)
+# invalid_large_string = "(([]){[()]}){(([]))" + "(([]){[})" * 50 + "(([]){})" + "(([]))(([]))}" + "({[}])"
+
+# pp valid_parentheses(string)
+pp is_valid_gsub_method(string)
